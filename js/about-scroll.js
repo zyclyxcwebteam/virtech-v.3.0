f9fa4c81c7b332feb962c9c2e3715946bf14ba1b
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-02-10 10:19:20 +0530

    Update contact us banner

--- js/about-scroll.js
+++ js/about-scroll.js
@@ -1,19 +1,21 @@
 $(function () {
     // init Scroll Magic    
+   
+    //$('.your-nav-container').find('a[href="' + location.pathname + '"]').parents('li').addClass('active');
     let controller = new ScrollMagic.Controller();
 
     // define movement of panels
     let slideAnimation = gsap.timeline()
         .from(".bg-title", { opacity: 1, duration: 2 })
         .from(".slide-1",{opacity:0,duration:2,delay:1,ease: "power1.out"})
-        .to("#slidesContainer", 4, { x: "-25%", })         
+        //.to("#slidesContainer", 4, { x: "-25%", })         
         .to("#slidesContainer", 4, { x: "-50%" })         
-        .to("#slidesContainer", 4, { x: "-75%" })         
+        //.to("#slidesContainer", 4, { x: "-75%" })         
     // create scene to pin and link animation
     new ScrollMagic.Scene({
         triggerElement: "#masterContainer",
         triggerHook: "onLeave",
-        duration: "800%",
+        duration: "200%",
         // reverse:false          
     })
         .setPin("#masterContainer")
