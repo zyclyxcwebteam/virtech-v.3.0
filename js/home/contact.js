40552df8afffbb137ab47dd2231f9ac6750b22a3
sandeepreddyguduru <reddy6969@gmail.com>
2020-02-11 15:19:01 +0530

    Updated Contact form Design

--- js/home/contact.js
+++ js/home/contact.js
@@ -33,5 +33,5 @@ gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})
 
 contactClose.addEventListener('click',function(event){
 event.preventDefault();
-gsap.to('#contactModal',{display:'none',height:'0px',duration:0.5})
+gsap.to('#contactModal',{display:'none',height:'100vh',duration:0.5})
 })
