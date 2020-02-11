dec61b19ef8b44b74bde63120b214d0d387c30a8
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-02-11 15:22:27 +0530

    Update navbar

--- js/home/contact.js
+++ js/home/contact.js
@@ -16,22 +16,3 @@ $(function(){
         .addTo(controller);
 
 });
-
-let contactButton = document.getElementById('contactButton');
-let contactLink = document.getElementById('contactLink');
-let contactClose = document.getElementById('contactClose');
- 
-contactLink.addEventListener('click',function(event){
-  event.preventDefault();
-  gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})    
-})
-
-contactButton.addEventListener('click',function(event){
-event.preventDefault();
-gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})    
-})
-
-contactClose.addEventListener('click',function(event){
-event.preventDefault();
-gsap.to('#contactModal',{display:'none',height:'0px',duration:0.5})
-})
