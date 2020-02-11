81e050eb9e4107ed5b411a154b0e49003238713a
sandeepreddyguduru <reddy6969@gmail.com>
2020-02-11 16:00:52 +0530

    added show / hide in contactus

--- js/home/contact.js
+++ js/home/contact.js
@@ -17,7 +17,7 @@ $(function(){
 
 });
 
-let contactButton = document.getElementById('contactButton');
+// let contactButton = document.getElementById('contactButton');
 let contactLink = document.getElementById('contactLink');
 let contactClose = document.getElementById('contactClose');
  
@@ -26,12 +26,12 @@ contactLink.addEventListener('click',function(event){
   gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})    
 })
 
-contactButton.addEventListener('click',function(event){
-event.preventDefault();
-gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})    
-})
+// contactButton.addEventListener('click',function(event){
+// event.preventDefault();
+// gsap.to('.contact-wrapper',{display:'block',height:'100vh',duration:0.5})    
+// })
 
 contactClose.addEventListener('click',function(event){
 event.preventDefault();
-gsap.to('#contactModal',{display:'none',height:'100vh',duration:0.5})
+gsap.to('#contactModal',{display:'none',height:'0px',duration:0.5})
 })
