d43625968ade64b837e9f5826f86b32c012d0839
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-02-12 10:01:04 +0530

    Fix home page pin height, update core values

--- js/home/services.js
+++ js/home/services.js
@@ -4,7 +4,7 @@ $(function(){
   // services slide animation
   let slideAnimation = gsap.timeline()       
       //.from("#masterContainer .service-bg-title", {duration: 1,opacity:1})
-      .to("#masterContainer .bg-title", {duration: 0.8,opacity:0.7,transform:'translate(0,0)',top:'90px',left:0,fontSize:'3.5rem'})
+      .to("#masterContainer .bg-title", {duration: 0.8,opacity:0.7,transform:'translate(0,0)',left:0,top:0,fontSize:'3.5rem'})
       .from("#slidesContainer .slide-1",{opacity:0,duration:1,ease: "power1.out"})
       
       //.from("#slidesContainer .title-card-1",{y:150,duration:1,ease: "power1.out"})
@@ -13,7 +13,7 @@ $(function(){
 
       //.from("#slidesContainer .title-card-2",{x:100,duration:1,opacity:0.95,ease: "power1.out"})
       //.from("#appCards .card",{x:100,duration:1,opacity:0.98,ease: "power1.out"})
-      .to("#slidesContainer", 4, {x: "-50%" })
+      .to("#slidesContainer", 4, {x: "-50%",  ease: Linear.easeNone})
       
       //.from("#slidesContainer .title-card-3",{x:100,duration:1,opacity:0.98,ease: "power1.out"})
       //.from("#infraCards .card",{x:100,duration:1,opacity:0.98,ease: "power1.out"})
@@ -25,12 +25,12 @@ $(function(){
   // create scene to pin and link animation
   new ScrollMagic.Scene({
       triggerElement: "#masterContainer",
-      triggerHook: "onLeave",
+      triggerHook:0.13,
       duration: "400%",                 
   })
       .setPin("#masterContainer")
       .setTween(slideAnimation)
-     // .addIndicators() // Indicators - remove on production
+      .addIndicators() // Indicators - remove on production
       .addTo(controller);    
 
 })
