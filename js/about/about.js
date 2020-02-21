6e8eac214197f446015314ca894f3c19344dc2f5
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-02-21 12:20:14 +0530

    Update career page, job details page

--- js/about/about.js
+++ js/about/about.js
@@ -1,10 +1,12 @@
 $(function(){     
 let controller = new ScrollMagic.Controller();
 let howWeDo = gsap.timeline();
-
+//howWeDo.fromTo('.do-content',{autoAlpha:1,duration:0.01})
+howWeDo.to('.do-content',{'--border-width':'0%',duration:0.001})
 howWeDo.fromTo('.do-content',{'--border-width':'0%'},{'--border-width':'20%',duration:2})
 howWeDo.fromTo('#do-icon-1',{autoAlpha:0,opacity:0,y:-55},{autoAlpha:1,opacity:1,y:0,duration:2},"-=2")
 howWeDo.fromTo('#do-text-1',{autoAlpha:0,y:55},{autoAlpha:1,y:0,duration:2},"-=2")
+//howWeDo.fromTo('.do-icon',{'--circle-opacity':0},{'--circle-opacity':1,duration:2},"-=2")
 
 howWeDo.fromTo('.do-content',{'--border-width':'20%'},{'--border-width':'40%',duration:2})
 howWeDo.fromTo('#do-icon-2',{autoAlpha:0,y:55},{y:0,autoAlpha:1,duration:2},"-=2")
@@ -25,9 +27,29 @@ howWeDo.fromTo('#do-text-5',{autoAlpha:0,y:55},{y:0,autoAlpha:1,y:0,duration:2},
 
 new ScrollMagic.Scene({
     triggerElement: "#howWeDoMaster",
-    triggerHook:"onEnter",                      
+    triggerHook:0.2,
+    reverse:false       
     })     
     .setTween(howWeDo)
-    .addIndicators()
-    .addTo(controller);         
-})
+    .addIndicators({name:'how we do'})
+    .addTo(controller);
+    
+    let missionBg = gsap.timeline()
+    missionBg.to('#missionMaster', { '--mission-bg':'#eeeef0', duration: 0.8 })
+    missionBg.fromTo(".mission-vission .about-text",{y:30,autoAlpha:0},{y:0,autoAlpha:1,duration:0.2})
+    
+    new ScrollMagic.Scene({
+        triggerElement:'#missionMaster',
+        triggerHook:0.25,
+        reverse:false
+    })
+    .setTween(missionBg)
+    .addIndicators({name:'mission-color'})
+    .addTo(controller)    
+
+
+
+
+    // end of init 
+});
+
