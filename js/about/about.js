4dde71a6fe8938ad85f143b6e983f87e3709f1d7
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-02-25 09:42:35 +0530

    Update how we do animation

--- js/about/about.js
+++ js/about/about.js
@@ -1,29 +1,35 @@
 $(function(){     
 let controller = new ScrollMagic.Controller();
+gsap.to('.do-content',{'--border-width':'0%',duration:0.001})
+
 let howWeDo = gsap.timeline();
 //howWeDo.fromTo('.do-content',{autoAlpha:1,duration:0.01})
 howWeDo.to('.do-content',{'--border-width':'0%',duration:0.001})
 howWeDo.fromTo('.do-content',{'--border-width':'0%'},{'--border-width':'20%',duration:2})
 howWeDo.fromTo('#do-icon-1',{autoAlpha:0,opacity:0,y:-55},{autoAlpha:1,opacity:1,y:0,duration:2},"-=2")
 howWeDo.fromTo('#do-text-1',{autoAlpha:0,y:55},{autoAlpha:1,y:0,duration:2},"-=2")
-//howWeDo.fromTo('.do-icon',{'--circle-opacity':0},{'--circle-opacity':1,duration:2},"-=2")
+howWeDo.fromTo('#do-icon-1',{'--circle-color':0},{'--circle-color':1,duration:0.02})
 
 howWeDo.fromTo('.do-content',{'--border-width':'20%'},{'--border-width':'40%',duration:2})
 howWeDo.fromTo('#do-icon-2',{autoAlpha:0,y:55},{y:0,autoAlpha:1,duration:2},"-=2")
 howWeDo.fromTo('#do-text-2',{autoAlpha:0,y:-55},{autoAlpha:1,y:0,duration:2},"-=2")
+howWeDo.fromTo('#do-icon-2',{'--circle-color':0},{'--circle-color':1,duration:0.02})
+
 
 howWeDo.fromTo('.do-content',{'--border-width':'40%'},{'--border-width':'60%',duration:2})
 howWeDo.fromTo('#do-icon-3',{autoAlpha:0,y:-55},{y:0,autoAlpha:1,duration:2},"-=2")
 howWeDo.fromTo('#do-text-3',{autoAlpha:0,y:55},{autoAlpha:1,y:0,duration:2},"-=2")
+howWeDo.fromTo('#do-icon-3',{'--circle-color':0},{'--circle-color':1,duration:0.02})
 
 howWeDo.fromTo('.do-content',{'--border-width':'60%'},{'--border-width':'80%',duration:2})
 howWeDo.fromTo('#do-icon-4',{autoAlpha:0,y:55},{y:0,autoAlpha:1,duration:2},"-=2")
 howWeDo.fromTo('#do-text-4',{autoAlpha:0,y:-55},{y:0,autoAlpha:1,y:0,duration:2},"-=2")
+howWeDo.fromTo('#do-icon-4',{'--circle-color':0},{'--circle-color':1,duration:0.02})
 
 howWeDo.fromTo('.do-content',{'--border-width':'80%'},{'--border-width':'100%',duration:2})
 howWeDo.fromTo('#do-icon-5',{autoAlpha:0,y:-55},{y:0,autoAlpha:1,duration:2},"-=2")
 howWeDo.fromTo('#do-text-5',{autoAlpha:0,y:55},{y:0,autoAlpha:1,y:0,duration:2},"-=2")
-//howWeDo.fromTo('.do-icon',{y:60,opacity:0,'--border-width':'0%'},{stagger:2,y:0,opacity:1,'--border-width':'100%',duration:1},"-=12")
+howWeDo.fromTo('#do-icon-5',{'--circle-color':0},{'--circle-color':1,duration:0.02})
 
 new ScrollMagic.Scene({
     triggerElement: "#howWeDoMaster",
