073edefdf19d4dcef2eb2c37b282f7a74c577b47
srikanthkanjarla <srikanthkanjarla@gmail.com>
2020-03-09 15:54:13 +0530

    Update navbar dropdown

--- js/shared/header.js
+++ js/shared/header.js
@@ -1,27 +1,55 @@
-$(function() { 
+$(function() {
   // $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
   // console.log($('#masterNavbar').find('a[href="' + location.pathname + '"]'));
-    // ! Navbar dropdown menu toggle
-    $("ul.dropdown-menu [data-toggle='dropdown']").on("mouseenter", function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show')             
-      $('.dropdown-submenu a').removeClass('active') 
-      $(this).addClass("active")             
-      $(this).siblings().toggleClass("show");           
+  // ! Navbar dropdown menu toggle
+  $("ul.dropdown-menu [data-toggle='dropdown']").on("mouseenter", function(
+    event
+  ) {
+    event.preventDefault();
+    event.stopPropagation();
+    $(this)
+      .parents(".dropdown-menu")
+      .first()
+      .find(".show")
+      .removeClass("show");
+    $(".dropdown-submenu a").removeClass("active");
+    $(this).addClass("active");
+    $(this)
+      .siblings()
+      .toggleClass("show");
 
     // TODO - CHECK this logic
-      if (!$(this).next().hasClass('show')) {
-        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-      }
-      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-        $('.dropdown-submenu .show').removeClass("show");
+    if (
+      !$(this)
+        .next()
+        .hasClass("show")
+    ) {
+      $(this)
+        .parents(".dropdown-menu")
+        .first()
+        .find(".show")
+        .removeClass("show");
+    }
+    $(this)
+      .parents("li.nav-item.dropdown.show")
+      .on("hidden.bs.dropdown", function(e) {
+        $(".dropdown-submenu .show").removeClass("show");
       });
-    });
-  
-    $("ul.dropdown-menu li:last-child").on("mouseenter", function(event) {
-      event.preventDefault();
-      event.stopPropagation(); 
-     $(this).parents('.dropdown-menu').first().find('.show').removeClass('show') 
-    })         
+  });
+
+  $("ul.dropdown-menu li:last-child").on("mouseenter", function(event) {
+    event.preventDefault();
+    // event.stopPropagation();
+    $(this)
+      .parents(".dropdown-menu")
+      .first()
+      .find(".show")
+      .removeClass("show");
+
+    $(this)
+      .parents(".dropdown-menu")
+      .first()
+      .find(".active")
+      .removeClass("active");
+  });
 });
